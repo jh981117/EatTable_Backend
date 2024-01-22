2e3eb6bbbfde67150497a1d95a58e8d566ae5d4c
HyungJu19 <HyungJu911@gmail.com>
2024-01-22 16:24:25 +0900

    #민호

---
+++ src/main/java/com/lec/spring/EatTableBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lec.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatTableBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatTableBackendApplication.class, args);
+    }
+
+}
