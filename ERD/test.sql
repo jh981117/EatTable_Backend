8940a1accafbd228b4dd410ca53bb3ca31033446
송민호 <hyungju911@gmail.com>
2024-02-01 21:42:11 +0900

    #민호  jwt 완성

--- ERD/test.sql
+++ ERD/test.sql
@@ -18,11 +18,11 @@ SELECT * FROM waiting;
 
 
 
+#
+# insert into user (id, username,birthdate,activated, password,nickName, name, bio , email)
+# values (2, 'admin', '910309','','$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi' ,'송', 'admin', 'ㅇㅇㄴ', 'mino030@naver.com');
 
-insert into user (id, username,birthdate,activated, password,nickName, name, bio , email)
-values (2, 'admin', '910309','','$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi' ,'송', 'admin', 'ㅇㅇㄴ', 'mino030@naver.com');
-
-insert into userrole (userId, roleId) values (2,1);
+# insert into userrole (userId, roleId) values (2,1);
 insert into userrole (userId, roleId) values (1,3);
 
 insert into role (id, roleName) values
--- src/test/java/com/lec/spring/service/UserServiceTest.java
+++ src/test/java/com/lec/spring/service/UserServiceTest.java
@@ -5,11 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Collections;
 import java.util.List;
 
-import static com.lec.spring.domain.RoleName.EAT_MEMBER;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 @SpringBootTest
 
