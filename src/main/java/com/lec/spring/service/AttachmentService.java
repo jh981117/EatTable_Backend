35dd46db01dc9d3fdce9120af093507020accaa4
ITmiran <151795831+ITmiran@users.noreply.github.com>
2024-02-22 23:05:52 +0900

    미란 리뷰 이미지 작업중...

--- src/main/java/com/lec/spring/service/AttachmentService.java
+++ src/main/java/com/lec/spring/service/AttachmentService.java
@@ -1,6 +1,7 @@
 package com.lec.spring.service;
 
 import com.lec.spring.domain.DTO.StoreReviewAttchmentDto;
+import com.lec.spring.domain.DTO.StoreReviewDto;
 import com.lec.spring.domain.PartnerAttachment;
 import com.lec.spring.domain.PartnerReviewAttachment;
 import com.lec.spring.domain.StoreReview;
@@ -48,7 +49,34 @@ public class AttachmentService {
     }
 
 
+    // 리뷰 이미지 수정
+    @Transactional
+    public void updateAttachments(Long reviewId, List<MultipartFile> files) {
+        System.out.println("리뷰아이디" + reviewId);
+        System.out.println("파일 : " + files);
 
+        StoreReview storeReview = storeReviewRepository.findById(reviewId).orElse(null);
 
+        // 새로운 이미지 업로드
+        for (MultipartFile file : files) {
+            String filename = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+            String path = s3Service.uploadFile(file);   // s3.
 
+            PartnerReviewAttachment attachment = PartnerReviewAttachment.builder()
+                    .filename(filename)
+                    .imageUrl(path)
+                    .isImage(true) // 여기서는 모든 파일을 이미지로 가정
+                    .storeReview(storeReview)
+                    .build();
+
+            partnerReviewAttachmentRepository.save(attachment);
+
+        }
+    }
+
+//    // 리뷰 이미지 삭제
+//    @Transactional
+//    public void deleteByReviewId(Long reviewId) {
+//        partnerReviewAttachmentRepository.deleteByReviewId(reviewId);
+//    }
 }
