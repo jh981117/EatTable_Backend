775aefcdc65cff7f8f19150c5cdbe32cb86b0d50
HyungJu19 <HyungJu911@gmail.com>
2024-02-26 10:59:34 +0900

    #민호

---
+++ src/main/java/com/lec/spring/domain/Tokens.java
@@ -0,0 +1,26 @@
+package com.lec.spring.domain;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Tokens {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    private String token;
+    private String refreshToken;
+}
